fd25bab59b43df9ea7008f3a3ade5c22131bf36e
fwefew123 <artem.andreev200012@mail.ru>
2023-12-01 00:15:58 +0600

    version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react"
 
-function App() {
+
+const App = () => {
+  const inputRef = useRef(null);
+  const [input, setInput] = useState('');
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={inputRef} type='text'/>
+      <button onClick={() => setInput(inputRef.current.value)}>asd</button>
+      <p>{input}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
